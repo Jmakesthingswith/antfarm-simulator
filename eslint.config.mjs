2c34ed7eb29d27d2ac58437c423773ec0e1f6629
joey10101 <61639249+joey10101@users.noreply.github.com>
2025-12-12 16:08:10 +0000

    Update Installed new Module 'deprecate' for ESLint, imported config and configured rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
+import deprecate from "eslint-plugin-deprecate";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+
+    plugins: {
+      deprecate,
+    },
+
+    extends: [js.configs.recommended],
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    rules: {
+      "deprecate/function": "warn",
+      "deprecate/import": "warn",
+    },
+  },
 ]);
